183654f4128e3535009fbecefe16131cf92c603f
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-31 02:34:57 +0530

    changing server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const server = "https://fitness-app-backend-cp21.onrender.com/api/v1"
-export const server = "http://localhost:5000/api/v1"
+export const server = "https://fitness-app-backend-cp21.onrender.com/api/v1"
+// export const server = "http://localhost:5000/api/v1"
